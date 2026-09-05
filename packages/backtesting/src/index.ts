3dbd1cb696f0bc37b25d9a5734dbac4ca949abf5
Rojan Dhimal <rojandhimal1@gmail.com>
2026-09-06 02:02:59 +1000

    feat: expose performance metrics

--- packages/backtesting/src/index.ts
+++ packages/backtesting/src/index.ts
@@ -3,4 +3,5 @@ export * from "./execution-stress.js";
 export * from "./execution-model.js";
 export * from "./signal-execution-adapter.js";
 export * from "./portfolio-accounting.js";
+export * from "./performance-metrics.js";
 export * from "./robustness.js";

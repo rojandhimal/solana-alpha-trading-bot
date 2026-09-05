1e2a11bead6dd3d125cb24ffc9953a99f2a2e89e
Rojan Dhimal <rojandhimal1@gmail.com>
2026-09-06 01:56:58 +1000

    feat: expose portfolio accounting

--- packages/backtesting/src/index.ts
+++ packages/backtesting/src/index.ts
@@ -2,4 +2,5 @@ export * from "./stress-testing.js";
 export * from "./execution-stress.js";
 export * from "./execution-model.js";
 export * from "./signal-execution-adapter.js";
+export * from "./portfolio-accounting.js";
 export * from "./robustness.js";

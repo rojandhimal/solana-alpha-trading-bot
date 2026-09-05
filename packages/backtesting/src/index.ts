8be2b9570b83fdb8a2e60bf9afc645259a7c639a
Rojan Dhimal <rojandhimal1@gmail.com>
2026-09-06 01:12:06 +1000

    feat: expose signal execution adapter

--- packages/backtesting/src/index.ts
+++ packages/backtesting/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./stress-testing.js";
 export * from "./execution-stress.js";
 export * from "./execution-model.js";
+export * from "./signal-execution-adapter.js";
 export * from "./robustness.js";

f2b8d73e1f4df156ff47afb5b1cd2f0729c28f98
Rojan Dhimal <rojandhimal1@gmail.com>
2026-09-08 14:33:38 +1000

    feat(market-data): export GeckoTerminal OHLCV source

--- packages/market-data/src/index.ts
+++ packages/market-data/src/index.ts
@@ -4,3 +4,4 @@ export * from "./eligibility.js";
 export * from "./historical-source.js";
 export * from "./historical-data-quality.js";
 export * from "./birdeye-ohlcv.js";
+export * from "./geckoterminal-ohlcv.js";
